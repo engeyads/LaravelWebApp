e22d4b6b85a68956634d92f0ffcb08b6b0e041be
Iyad Sammour <45532535+engeyads@users.noreply.github.com>
2022-06-30 19:18:37 +0300

    new Template Repo
    This is a laravel tempalte that can be updated with new content

---
+++ resources/lang/ar/blog.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | blog Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given for the blog.
+    |
+    */
+
+    /*<!-- blog page--> */
+    'title' => 'مدونة تركيا شقة الاحلام | كاميران - المدونة',
+    'titleForSEO' => 'تركيا شقة الاحلام',
+    'subtitle' => 'مدونة تركيا شقة الاحلام',
+    'keywords' => 'سوق العقار, تركيا شقة الاحلام من نحن',
+    'description' => 'تركيا شقة الاحلام | كاميران',
+];
---
+++ resources/lang/en/home.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | home Page Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given for the home page.
+    |
+    */
+
+    /*<!-- home page--> */
+    'direction' => 'ltr',
+    'title' => 'Salametcom | Home',
+    'subtitle' => 'Home Page',
+    'services' => 'SERVICES',
+    'services_text' => '“Customer service shouldn’t just be a department; it should be the entire company.” – Tony Hsieh',
+    'titleForSEO' => 'Treatment abroad - Treatment in Turkey - Treatment - in Istanbul - Health in Turkey',
+    'keywords' => 'Treatment abroad - Salametcom Who we are',
+    'description' => 'Salametcom',
+];
